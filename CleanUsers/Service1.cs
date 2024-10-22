71cea78d010d96001565be5854215b56a0c8757b
Sammy Seoloane <159002009+Sammy-Developer@users.noreply.github.com>
2024-10-22 16:10:51 +0200

    Add files via upload

---
+++ CleanUsers/Service1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Web.Services;
+using CleanUsers.CleanActveUsers; 
+
+
+namespace CleanUsers
+{
+    public partial class Service1 : ServiceBase
+    {
+        private System.Timers.Timer timer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Initialize the timer
+            timer = new System.Timers.Timer();
+            timer.Interval = 60000; // 1 minute intervals
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Start();
+        }
+
+
+        private void OnElapsedTime(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                // Create a web service client
+
+                CleanActveUsers.Service serviceClient = new CleanActveUsers.Service();
+                
+                   
+                     serviceClient.CleanActiveUser(); //CleanActiveUser
+
+                serviceClient.RecordLoginStats(); 
+                            
+            }
+            catch (Exception ex)
+            {
+                // Log the error 
+                WriteErrorLog(ex.ToString(), "ErrorLog.txt");
+
+            }   }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+
+
+  
+        public void WriteErrorLog(string Message, string FileName)
+        {
+
+            //Uncomment below to disable writing any log information of errors into the log file.
+            StreamWriter streamWriter = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\" + FileName + ".txt", true);
+            try
+            {
+                streamWriter.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                if (streamWriter != null)
+                {
+                    streamWriter.Close();
+                }
+            }
+        }
+
+
+    }
+}
